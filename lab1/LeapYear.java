84ee05472a088c6c600ff1ca2c18a906a5e1e4fc
airbust <airbust@qq.com>
2018-10-04 20:25:43 +0800

    lab1 modified

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,19 +6,19 @@ public class LeapYear {
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
-    private static boolean isLeapYear(int year) {
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-            return true;
-        }
-        return false;
-    }
-
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
