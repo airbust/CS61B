3737f5d6b60166793d277ddc8837824c7d5dd125
airbust <airbust@qq.com>
2018-10-04 20:30:21 +0800

    fixed

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,11 @@
 public class HelloNumbers {
 	public static void main(String[] args) {
 		int x = 0;
-		while (x < 10) {
+		int increment = 1;
+		while (increment <= 10) {
 			System.out.print(x + " ");
-			x = x + 1;
+			x += increment;
+			increment++;
 		}
 	}
 }
