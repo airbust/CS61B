0102e30f5c91a9153785a9a952f86e3d1c51bc66
airbust <airbust@qq.com>
2019-01-25 22:44:37 +0800

    proj1gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,21 +3,43 @@ import org.junit.Test;
 
 public class TestArrayDequeGold {
     @Test
-    public void addRemoveTest() {
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<Integer>();
-        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<Integer>();
+    public void arrayDequeTest() {
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
 
-        String message = "";
-        for (int i = 0; i < 10; i++) {
-            sad.addLast(i);
-            ads.addLast(i);
-            message += "addLast(" + i + ")\naddLast(" + i + ")\n";
-        }
+        for (int i = 0; i < 100; i++) {
+            int flag = StdRandom.uniform(0, 4);
+            Integer val = Integer.valueOf(StdRandom.uniform(100));
 
-        for (int i = 0; i < 5; i++) {
-            message += "removeLast()\n";
-            assertEquals(message, ads.removeLast(), sad.removeLast());
+            if (flag == 0) {
+                System.out.println("addFirst(" + val + ")");
+                ads.addFirst(val);
+                sad.addFirst(val);
+            } else {
+                if (flag == 1) {
+                    System.out.println("addLast(" + val + ")");
+                    ads.addLast(val);
+                    sad.addLast(val);
+                } else {
+                    if (flag == 2) {
+                        if (!ads.isEmpty()) {
+                            System.out.println("removeFirst()");
+                            assertEquals(ads.isEmpty(), sad.isEmpty());
+                            Integer exp = ads.removeFirst();
+                            Integer ac = sad.removeFirst();
+                            assertEquals("removeFirst()", exp, ac);
+                        }
+                    } else {
+                        if (!ads.isEmpty()) {
+                            System.out.println("removeLast()");
+                            assertEquals(ads.isEmpty(), sad.isEmpty());
+                            Integer exp = ads.removeLast();
+                            Integer act = sad.removeLast();
+                            assertEquals("removeLast()", exp, act);
+                        }
+                    }
+                }
+            }
         }
     }
-
 }
